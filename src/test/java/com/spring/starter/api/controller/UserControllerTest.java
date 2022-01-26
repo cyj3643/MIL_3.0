429a7b785d1f540e63b7c387368409e085802066
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-01-26 16:07:51 +0900

    feat: displayUserInfo test 추가

--- src/test/java/com/spring/starter/api/controller/UserControllerTest.java
+++ src/test/java/com/spring/starter/api/controller/UserControllerTest.java
@@ -1,14 +1,32 @@
 package com.spring.starter.api.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-class UserControllerTest {
+import com.spring.starter.BaseTest;
+
+@DisplayName("User Controller Test")
+class UserControllerTest extends BaseTest {
 
 	@Test
-	void displayUserInfo() {
+	@DisplayName("유저 정보를 스트링으로 반환(성공)")
+	void displayUserInfo() throws Exception {
+		//Given
+		String name = "효택";
+		String address = "경기도 어딘가";
 
+		//When
+		ResultActions perform = this.mockMvc.perform(
+			MockMvcRequestBuilders.get("/").param("name", name).param("address", address));
+
+		//Then
+		perform.andExpect(status().isOk())
+			.andExpect(content().string("효택님의 주소는 경기도 어딘가입니다."));
 	}
 
 	@Test

921307fba21ec050a1e6d854752f4fca63204977
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-02-24 00:00:05 +0900

    feat: 기존 response BaseResponse로 변경

--- src/main/java/com/spring/starter/api/controller/UserController.java
+++ src/main/java/com/spring/starter/api/controller/UserController.java
@@ -15,6 +15,7 @@ import com.spring.starter.api.service.AreaService;
 import com.spring.starter.api.service.UserService;
 import com.spring.starter.common.model.BaseResponse;
 import com.spring.starter.db.entity.Area;
+import com.spring.starter.db.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,29 +29,32 @@ public class UserController {
 	private final PasswordEncoder passwordEncoder;
 
 	@PostMapping("/signup")
-	public ResponseEntity<String> signUpUser(@Valid @RequestBody SingUpUserReq singUpUserReq) {
+	public ResponseEntity<? extends BaseResponse> signUpUser(@Valid @RequestBody SingUpUserReq singUpUserReq) {
 		if (userService.isExistEmail(singUpUserReq.getEmail()))
-			return ResponseEntity.status(409).body("이미 존재하는 이메일입니다.");
+			return ResponseEntity.status(409).body(new BaseResponse("이미 존재하는 이메일입니다.", 409));
 		if (userService.isExistUserId(singUpUserReq.getUserId()))
-			return ResponseEntity.status(409).body("이미 존재하는 아이디입니다.");
+			return ResponseEntity.status(409).body(new BaseResponse("이미 존재하는 아이디입니다.", 409));
 
 		Area area = null;
 		if (singUpUserReq.getArea() != null) {
 			area = areaService.nameToArea(singUpUserReq.getArea());
 			if (area == null)
-				return ResponseEntity.status(400).body("Area의 값이 잘못됐습니다.");
+				return ResponseEntity.status(400).body(new BaseResponse("Area의 값이 잘못됐습니다.", 400));
 		}
 
 		// ToDo(천천히) 학생 Area -> 자동화
 		String encode = passwordEncoder.encode(singUpUserReq.getPassword());
 		userService.save(singUpUserReq.toUserEntity(area, encode));
 
-		return ResponseEntity.status(201).body("회원가입에 성공하였습니다.");
+		return ResponseEntity.status(201).body(new BaseResponse("회원가입에 성공하였습니다.", 201));
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<? extends BaseResponse> login(@Valid @RequestBody LoginReq loginReq) {
-		System.out.println(loginReq);
+		User byId = userService.findById(loginReq.getUserId());
+		if (!passwordEncoder.matches(loginReq.getPassword(), byId.getPassword())) {
+			return ResponseEntity.status(400).body(new BaseResponse("비밀번호가 일치하지 않습니다.", 400));
+		}
 		return null;
 	}
 

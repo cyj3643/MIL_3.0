3bb010a3c3c778e35e8623ef4f87e7f54708598e
wnshrla <wnshrla@naver.com>
2022-03-03 20:19:54 +0900

    feat: refreshToken Entity

--- src/main/java/com/spring/starter/db/entity/RefreshToken.java
+++ src/main/java/com/spring/starter/db/entity/RefreshToken.java
@@ -3,7 +3,6 @@ package com.spring.starter.db.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -14,18 +13,18 @@ import javax.persistence.Id;
 public class RefreshToken {
 
     @Id
-    private String key;
-    private String value;
+    private String refreshKey;
+    private String refreshValue;
 
     public RefreshToken updateValue(String token) {
-        this.value = token;
+        this.refreshValue = token;
         return this;
     }
 
     @Builder
     public RefreshToken(String key, String value) {
-        this.key = key;
-        this.value = value;
+        this.refreshKey = key;
+        this.refreshValue = value;
     }
     // RDB 로 구현하면 생성/수정 시간 컬럼 추가하여 배치 작업으로 만료된 토큰들 삭제
 }

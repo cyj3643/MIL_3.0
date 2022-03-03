a1d16027ce6a5c4eced1c4b932112eb24575e296
wnshrla <wnshrla@naver.com>
2022-03-03 20:23:49 +0900

    feat: Dto

---
+++ src/main/java/com/spring/starter/api/response/index/TokenResponseDto.java
@@ -0,0 +1,15 @@
+package com.spring.starter.api.response.index;
+
+import com.spring.starter.common.model.BaseResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDto {
+    private String accessToken;
+    private String refreshToken;
+
+}

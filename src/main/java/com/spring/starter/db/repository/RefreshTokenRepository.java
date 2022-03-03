8afd251463f82d56d61a21dd31bfed408d772c22
wnshrla <wnshrla@naver.com>
2022-03-03 20:19:37 +0900

    feat: refreshToken Repo

--- src/main/java/com/spring/starter/db/repository/RefreshTokenRepository.java
+++ src/main/java/com/spring/starter/db/repository/RefreshTokenRepository.java
@@ -2,10 +2,12 @@ package com.spring.starter.db.repository;
 
 import com.spring.starter.db.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> findByKey(String key);
+    Optional<RefreshToken> findByRefreshKey(String key);
 }

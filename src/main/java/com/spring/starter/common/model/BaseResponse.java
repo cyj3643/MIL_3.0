329c1b545f3c61a18e70f0ca6192d0172e06851d
wnshrla <wnshrla@naver.com>
2022-03-09 15:06:28 +0900

    feat: setter 추가

--- src/main/java/com/spring/starter/common/model/BaseResponse.java
+++ src/main/java/com/spring/starter/common/model/BaseResponse.java
@@ -1,8 +1,10 @@
 package com.spring.starter.common.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class BaseResponse{
 	String msg = null;
 	Integer status = null;

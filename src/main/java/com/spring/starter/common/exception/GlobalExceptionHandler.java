4555b28799b4e582ceb46fd8af5fc997fb02d5c6
wnshrla <wnshrla@naver.com>
2022-03-09 15:07:23 +0900

    feat: JwtException 추가

--- src/main/java/com/spring/starter/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/starter/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.spring.starter.common.exception;
 import java.util.Arrays;
 import java.util.Objects;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,17 +19,21 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
 	protected ResponseEntity<BaseResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		String defaultMessage = Objects.requireNonNull(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());;
+		String defaultMessage = Objects.requireNonNull(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 		log.error("handle Method Argument Not Valid Exception : {}", defaultMessage);
 		return ResponseEntity.status(400).body(new BaseResponse(defaultMessage, 400));
 	}
 
 	@ExceptionHandler(value = Exception.class)
 	public ResponseEntity<BaseResponse> handleException(Exception e) {
-		log.error("Internal Error Trace : {} ",Arrays.toString(e.getStackTrace()));
-		log.error("Error Message : {}",e.getMessage());
+		log.error("Internal Error Trace : {} ", Arrays.toString(e.getStackTrace()));
+		log.error("Error Message : {}", e.getMessage());
 		return ResponseEntity.status(500).body(new BaseResponse(e.getMessage(), 500));
 	}
 
+	@ExceptionHandler(value = JwtException.class)
+	public ResponseEntity<BaseResponse> handleJwtException(Exception e) {
+		return ResponseEntity.status(401).body(new BaseResponse(e.getMessage(), 401));
+	}
 }
 

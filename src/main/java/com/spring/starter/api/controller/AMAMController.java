43f1953df5c87e87ec62343e9b387286e58d3497
KimTaeSeok <sukyuree@blueprint-lab.com>
2022-03-13 21:31:31 +0900

    FEAT : AMAM 컨트롤러

---
+++ src/main/java/com/spring/starter/api/controller/AMAMController.java
@@ -0,0 +1,43 @@
+package com.spring.starter.api.controller;
+
+import com.spring.starter.api.request.user.PostamamReq;
+import com.spring.starter.api.service.AMAMService;
+import com.spring.starter.common.model.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/amam")
+public class AMAMController {
+    private final AMAMService amamService;
+
+    @PostMapping("/board")
+    public ResponseEntity<? extends BaseResponse> PostAMAM(@Valid @RequestBody PostamamReq postamamReq){
+        amamService.save(postamamReq);
+        return ResponseEntity.status(201).body(new BaseResponse("글 작성 완료.", 201));
+    }
+
+    @GetMapping("/board")
+    public ResponseEntity GetAllAMAM(@Valid Pageable pageable){
+        return ResponseEntity.status(201).body(amamService.get_all(pageable));
+    }
+
+    @GetMapping("/board/{title}")
+    public ResponseEntity GetAMAM(@Valid @PathVariable(value = "title") String title){
+        return ResponseEntity.status(201).body(amamService.get_content(title));
+    }
+
+    @GetMapping("/board/area/{areaname}")
+    public ResponseEntity GetAreaAMAM(@Valid Pageable pageable, @PathVariable (value = "areaname")String areaName){
+        return ResponseEntity.status(201).body(amamService.getarea_all(areaName,pageable));
+    }
+
+}

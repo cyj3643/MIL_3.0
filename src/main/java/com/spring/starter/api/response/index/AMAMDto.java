a6b250c2af4d50f911e4a67024d8bdf87a453517
KimTaeSeok <sukyuree@blueprint-lab.com>
2022-03-13 21:32:32 +0900

    FEAT : 게시판, 게시글 용 dto

---
+++ src/main/java/com/spring/starter/api/response/index/AMAMDto.java
@@ -0,0 +1,28 @@
+package com.spring.starter.api.response.index;
+
+
+import com.spring.starter.db.entity.AMAM;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class AMAMDto {
+    String title;
+    Date createdAt;
+    Long no;
+    String content;
+    String area;
+    String user;
+    Long viewCount;
+
+    public AMAMDto(AMAM amam){
+        this.title=amam.getTitle();
+        this.createdAt=amam.getCreatedAt();
+        this.no=amam.getNo();
+        this.area=amam.getArea().getName();
+        this.user=amam.getUser().getName();
+        this.viewCount=amam.getViewCount();
+        this.content=amam.getContent();
+    }
+}

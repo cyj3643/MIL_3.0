d70b975a6589bc0ad2aa67895850b2af7619cec4
wnshrla <wnshrla@naver.com>
2022-03-29 14:50:47 +0900

    feat: thumbnail -> project 명세서 순으로 변경

--- src/main/java/com/spring/starter/api/controller/ProjectController.java
+++ src/main/java/com/spring/starter/api/controller/ProjectController.java
@@ -1,7 +1,7 @@
 package com.spring.starter.api.controller;
 
 import com.spring.starter.api.request.user.ProjectDto;
-import com.spring.starter.api.request.user.ProjectFileDto;
+import com.spring.starter.api.response.ProjectResDto;
 import com.spring.starter.api.response.index.ProjectRes;
 import com.spring.starter.api.service.ProjectService;
 import com.spring.starter.api.service.S3Service;
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,32 +31,34 @@ public class ProjectController {
     private String awsUrl;
 
     @GetMapping("/all")
-    public ResponseEntity projectAll() {
-        return ResponseEntity.ok(projectService.getAllProject().stream().map(file -> new ProjectRes(file, awsUrl)).collect(Collectors.toList()));
+    public ResponseEntity<? extends BaseResponse> projectAll() {
+        List<ProjectResDto> projectResDtoList = projectService.getAllProject().stream().map(project -> new ProjectResDto(project)).collect(Collectors.toList());
+        for (ProjectResDto projectResDto : projectResDtoList) {
+            projectResDto.setFile(awsUrl + projectResDto.getFile());
+        }
+        return ResponseEntity.status(201).body(new ProjectRes("모든 프로젝트를 가져왔습니다.", 201, projectResDtoList));
     }
 
     @GetMapping("/{job}")
-    public ResponseEntity projectByJob(@PathVariable String job) {
+    public ResponseEntity<? extends BaseResponse> projectByJob(@PathVariable String job) {
         Jobs findJob = jobsRepository.findByNameContaining(job);
-        return ResponseEntity.ok(projectService.getProjectByJobs(findJob.getId()).stream().map(file -> new ProjectRes(file, awsUrl)).collect(Collectors.toList()));
+        List<ProjectResDto> projectResDtoListByJob = projectService.getProjectByJobs(findJob.getId()).stream().map(project -> new ProjectResDto(project)).collect(Collectors.toList());
+        for (ProjectResDto projectResDto : projectResDtoListByJob) {
+            projectResDto.setFile(awsUrl + projectResDto.getFile());
+        }
+        return ResponseEntity.status(201).body(new ProjectRes("직업에 해당하는 프로젝트를 가져왔습니다.", 201, projectResDtoListByJob));
     }
 
-    @PostMapping("/thumbnail/{id}")
-    public ResponseEntity<? extends BaseResponse> thumbnail(@RequestParam("images") MultipartFile multipartFile, @PathVariable Long id) throws IOException {
+    @PostMapping("/thumbnail")
+    public ResponseEntity<? extends BaseResponse> thumbnail(@RequestParam("images") MultipartFile multipartFile) throws IOException {
         // base url 떼고 file path
         String upload = s3Service.upload(multipartFile);
-        ProjectFileDto projectFileDto = new ProjectFileDto();
-        projectFileDto.setFile(upload);
-        projectFileDto.setDivision(0);
-        projectFileDto.setProjectId(id);
-        projectService.saveProjectFile(projectFileDto);
-
-        return ResponseEntity.status(201).body(new BaseResponse("성공적으로 업로드 했습니다!", 201));
+        return ResponseEntity.status(201).body(new BaseResponse(upload, 201));
     }
 
     @PostMapping("/upload")
-    public ResponseEntity upload(@Valid @RequestBody ProjectDto projectDto) {
-        Long saveProject = projectService.saveProject(projectDto);
-        return ResponseEntity.ok(saveProject);
+    public ResponseEntity<? extends BaseResponse> upload(@Valid @RequestBody ProjectDto projectDto) {
+        projectService.saveProject(projectDto);
+        return ResponseEntity.status(201).body(new BaseResponse("업로드를 성공하였습니다.", 201));
     }
 }

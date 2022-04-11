18c301be088f14f55a2daedb9b5bdc8ceb87ff6b
wnshrla <wnshrla@naver.com>
2022-04-11 21:43:15 +0900

    feat: upload(Industry, Project) 구분(수정 부분때문에)

--- src/main/java/com/spring/starter/api/service/S3Service.java
+++ src/main/java/com/spring/starter/api/service/S3Service.java
@@ -8,6 +8,11 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.spring.starter.db.entity.IndustryVideo;
+import com.spring.starter.db.repository.IndustryVideoRepository;
+import com.spring.starter.db.repository.ProjectFileRepository;
+import com.spring.starter.db.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,12 +20,16 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class S3Service {
 
+    private final IndustryVideoRepository industryVideoRepository;
+    private final ProjectFileRepository projectFileRepository;
     private AmazonS3 S3Client;
 
     @Value("${cloud.aws.credentials.access-key}")
@@ -47,7 +56,14 @@ public class S3Service {
                 .build();
     }
 
-    public String upload(MultipartFile file) throws IOException {
+    public String uploadIndustry(Long videoId, MultipartFile file) throws IOException {
+        if (videoId != null) {
+            IndustryVideo findIndustry = industryVideoRepository.findById(videoId).orElse(null);
+            String thumbnail = findIndustry.getThumbnail();
+            if (S3Client.doesObjectExist(bucket, thumbnail)) {
+                S3Client.deleteObject(bucket, thumbnail);
+            }
+        }
         String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         ObjectMetadata metadata = new ObjectMetadata();
@@ -58,6 +74,25 @@ public class S3Service {
         return substring;
     }
 
+    public String uploadProject(Long projectId, MultipartFile file) throws IOException {
+        if (projectId != null) {
+            String findProject = projectFileRepository.findById(projectId).orElse(null).getFile();
+            if (S3Client.doesObjectExist(bucket, findProject)) {
+                S3Client.deleteObject(bucket, findProject);
+            }
+        }
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        S3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        String substring = fileName.substring(fileName.lastIndexOf("/") + 1);
+        return substring;
+    }
+
+    public void deleteFile(String file) {
+        S3Client.deleteObject(bucket, file);
+    }
 
 }

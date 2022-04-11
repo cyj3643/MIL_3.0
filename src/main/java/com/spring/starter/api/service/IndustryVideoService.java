88c96f0318b24b85f56c2ddc10f0e91584a40e83
wnshrla <wnshrla@naver.com>
2022-04-11 21:39:59 +0900

    feat: post부분 admin으로 이동

--- src/main/java/com/spring/starter/api/service/IndustryVideoService.java
+++ src/main/java/com/spring/starter/api/service/IndustryVideoService.java
@@ -1,12 +1,8 @@
 package com.spring.starter.api.service;
 
-import com.spring.starter.api.request.user.IndustryVideoReq;
 import com.spring.starter.db.entity.IndustryVideo;
-import com.spring.starter.db.entity.Jobs;
 import com.spring.starter.db.repository.IndustryVideoRepository;
-import com.spring.starter.db.repository.JobsRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,17 +12,6 @@ import java.util.List;
 public class IndustryVideoService {
 
     private final IndustryVideoRepository industryVideoRepository;
-    private final JobsRepository jobsRepository;
-
-    public Long saveVideo(IndustryVideoReq industryVideoReq) {
-        String jobs = industryVideoReq.getJobs();
-        Jobs findJob = jobsRepository.findByNameContaining(jobs);
-        IndustryVideo industryVideo = new IndustryVideo();
-        industryVideo.setJobs(findJob);
-        BeanUtils.copyProperties(industryVideoReq, industryVideo, "jobs");
-        IndustryVideo save = industryVideoRepository.save(industryVideo);
-        return save.getId();
-    }
 
     public List<IndustryVideo> getAll() {
         return industryVideoRepository.findAll();

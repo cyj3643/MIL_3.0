4e926ad3ffb8e0151e0b1e82a5867b031b155638
wnshrla <wnshrla@naver.com>
2022-04-19 10:48:23 +0900

    feat: 관리자에게 모든 비디오와 특정비디오 보여주기 추가

--- src/main/java/com/spring/starter/api/admin/controller/IndustryVideoAdminController.java
+++ src/main/java/com/spring/starter/api/admin/controller/IndustryVideoAdminController.java
@@ -1,34 +1,55 @@
 package com.spring.starter.api.admin.controller;
 
+import com.spring.starter.api.admin.response.IndustryVideoAdminDto;
+import com.spring.starter.api.admin.response.index.IndustryVideoAdminInfoRes;
+import com.spring.starter.api.admin.response.index.IndustryVideoAdminRes;
 import com.spring.starter.api.admin.service.IndustryVideoAdminService;
 import com.spring.starter.api.request.user.IndustryVideoReq;
 import com.spring.starter.api.service.S3Service;
 import com.spring.starter.common.model.BaseResponse;
 import com.spring.starter.db.entity.IndustryVideo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/admin/industry")
+@RequestMapping("/admin/industry/video")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 public class IndustryVideoAdminController {
 
     private final S3Service s3Service;
     private final IndustryVideoAdminService industryVideoAdminService;
+    @Value("${aws.url}")
+    private String awsURL;
+
+    @GetMapping("")
+    public ResponseEntity<? extends BaseResponse> getAllVideo() {
+        List<IndustryVideoAdminDto> industryVideoInfoResDtoList = industryVideoAdminService.getAll().stream().map(video -> new IndustryVideoAdminDto(video)).collect(Collectors.toList());
+        return ResponseEntity.status(200).body(new IndustryVideoAdminRes("모든 비디오를 가져왔습니다.", 200, industryVideoInfoResDtoList));
+    }
+
+    @GetMapping("/{videoId}")
+    public ResponseEntity<? extends BaseResponse> getVideoById(@PathVariable Long videoId) {
+        IndustryVideo video = industryVideoAdminService.findVideo(videoId);
+        return ResponseEntity.status(200).body(new IndustryVideoAdminInfoRes("videoId에 의해 비디오를 가져왔습니다.", 200, video));
+    }
+
 
     @PostMapping(value = {"/thumbnail", "/thumbnail/{videoId}"})
-    public ResponseEntity<? extends BaseResponse> getS3Url(@PathVariable(required = false) Long videoId, @RequestParam("images") MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<? extends BaseResponse> postS3Url(@PathVariable(required = false) Long videoId, @RequestParam("images") MultipartFile multipartFile) throws IOException {
         String upload = s3Service.uploadIndustry(videoId, multipartFile);
         return ResponseEntity.status(201).body(new BaseResponse(upload, 201));
     }
 
-    @PostMapping("/video")
+    @PostMapping("")
     public ResponseEntity<? extends BaseResponse> uploadVideo(@RequestBody IndustryVideoReq industryVideoReq) {
         industryVideoAdminService.saveVideo(industryVideoReq);
         return ResponseEntity.status(201).body(new BaseResponse("성공적으로 업로드 하였습니다.", 201));

c6213e4bf2043e74e7a4febfe24c85ec45180b17
wnshrla <wnshrla@naver.com>
2022-04-19 10:50:11 +0900

    feat: InfoDto 에 Email & Area 추가

--- src/main/java/com/spring/starter/api/response/index/InfoDto.java
+++ src/main/java/com/spring/starter/api/response/index/InfoDto.java
@@ -10,10 +10,14 @@ public class InfoDto extends BaseResponse {
 
     private String name;
     private Integer studentId;
+    private String email;
+    private String area;
 
     public InfoDto(String msg, Integer status, User user) {
         super(msg, status);
         this.name = user.getName();
         this.studentId = user.getStudentId();
+        this.email = user.getEmail();
+        this.area = user.getArea().getName();
     }
 }

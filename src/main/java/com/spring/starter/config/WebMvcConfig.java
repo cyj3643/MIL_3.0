de5f6bff07f178cc66c3b8f09e70ed162b479ffe
wnshrla <wnshrla@naver.com>
2022-05-03 12:50:06 +0900

    feat: ResourceHandler 이용하여 local 저장소 접근

--- src/main/java/com/spring/starter/config/WebMvcConfig.java
+++ src/main/java/com/spring/starter/config/WebMvcConfig.java
@@ -1,22 +1,30 @@
 package com.spring.starter.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.spring.starter.common.interceptor.LogInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
+import static com.spring.starter.common.util.constants.UploadConstant.Industry_BASE_URL;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final LogInterceptor logInterceptor;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/industryImg/**")
+				.addResourceLocations("file:///" + Industry_BASE_URL);
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {

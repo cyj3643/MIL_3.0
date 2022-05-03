1279b2ae6405804c189a5997e48f8580f1d3b64e
wnshrla <wnshrla@naver.com>
2022-05-03 12:47:33 +0900

    feat: 썸네일 저장 방식 변경(S3 -> Local)

--- src/main/java/com/spring/starter/api/admin/Service/IndustryVideoAdminService.java
+++ src/main/java/com/spring/starter/api/admin/Service/IndustryVideoAdminService.java
@@ -9,8 +9,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
+
+import static com.spring.starter.common.util.constants.UploadConstant.Industry_BASE_URL;
+
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +25,22 @@ public class IndustryVideoAdminService {
 
     private final IndustryVideoRepository industryVideoRepository;
     private final JobsRepository jobsRepository;
+
+    public String saveThumbnail(Long videoId, MultipartFile multipartFile) throws IOException {
+        String basePath = Industry_BASE_URL;
+        String fileName = UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+        String filePath = basePath + fileName;
+        File dest = new File(filePath);
+        multipartFile.transferTo(dest);
+
+        if (videoId != null) {
+            IndustryVideo industryVideo = industryVideoRepository.findById(videoId).orElse(null);
+            String thumbnail = industryVideo.getThumbnail();
+            File deleteFile = new File(thumbnail);
+            deleteFile.delete();
+        }
+        return fileName;
+    }
 
     public Long saveVideo(IndustryVideoReq industryVideoReq) {
         String jobs = industryVideoReq.getJobs();
@@ -27,6 +50,11 @@ public class IndustryVideoAdminService {
         BeanUtils.copyProperties(industryVideoReq, industryVideo, "jobs");
         IndustryVideo save = industryVideoRepository.save(industryVideo);
         return save.getId();
+    }
+
+    public void deleteThumbnail(String thumbnail) {
+        File file = new File(Industry_BASE_URL + "/" + thumbnail);
+        file.delete();
     }
 
     public IndustryVideo findVideo(Long videoId) {
